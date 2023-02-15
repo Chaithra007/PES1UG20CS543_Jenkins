dd23107c72783cd3a0209a365371efcf34fdacb7
Chaithra <chaithrausha2002@gmail.com>
2023-02-15 18:06:30 +0530

    Commit successful

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins from PES1UG20CS543\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
